64530da3e1565bcc4923b18c192c593f4aaebfdd
GustavoRPaes <gustavo.rp@aluno.ifsc.edu.br>
2022-01-27 20:43:23 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/gustavo/projeto/entities/User.java
+++ src/main/java/com/gustavo/projeto/entities/User.java
@@ -1,18 +1,25 @@
 package com.gustavo.projeto.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class User implements Serializable{
+@Table(name = "tb_user")
+public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -20,7 +27,11 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
-	
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> order = new ArrayList<>();
+
 	public User() {
 	}
 
@@ -73,6 +84,10 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
+	public List<Order> getOrder() {
+		return order;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -89,6 +104,4 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 }
