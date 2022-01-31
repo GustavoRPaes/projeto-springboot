9e153c9f873c17084b35ba6df8022246f60be7a0
GustavoRPaes <gustavo.rp@aluno.ifsc.edu.br>
2022-01-31 12:48:54 -0300

    Payment, one-to-one association

--- src/main/java/com/gustavo/projeto/config/TestConfig.java
+++ src/main/java/com/gustavo/projeto/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.gustavo.projeto.entities.Category;
 import com.gustavo.projeto.entities.Order;
 import com.gustavo.projeto.entities.OrderItem;
+import com.gustavo.projeto.entities.Payment;
 import com.gustavo.projeto.entities.Product;
 import com.gustavo.projeto.entities.User;
 import com.gustavo.projeto.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
