b9611eb378c565412b101bf53f77d1dffb339acf
GustavoRPaes <gustavo.rp@aluno.ifsc.edu.br>
2022-02-01 12:47:31 -0300

    Exception handling - findById

---
+++ src/main/java/com/gustavo/projeto/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.gustavo.projeto.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. id " + id);
+	}
+
+}
